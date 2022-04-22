f9ef70ad5807ee3fa125576fefc17a1999dd8574
JeongHun <21900413@handong.ac.kr>
2022-04-23 01:05:03 +0900

    loaddata

--- manager.c
+++ manager.c
@@ -54,14 +54,29 @@ void saveData(Product p[], int count){
 
 //파일에서 데이터 불러오는 함수
 int loadData(Product *p){
-	int count=0;
+	int count=0, i = 0;
 	FILE*fp;
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
 
+	fp = fopen("product.txt", "r");
+	if(fp == NULL)
+	{
+		printf("파일이 존재하지 않음!");
+		return 0;
+	}
 
+	for(; i < 100; i++)
+	{
+		fscanf(fp, "%d", &p[i].weight);
+		if(feof(fp)) break;
+		fscanf(fp, "%d", &p[i].price);
+		fscanf(fp, "%[^\n]s", p[i].name);
+	}
+	fclose(fp);
 
 
 	printf("=> 로딩 성공!\n");
+	count = i;
 	return count;
 }
